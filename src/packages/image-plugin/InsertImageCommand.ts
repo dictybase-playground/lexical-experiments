1385b50ca526f05584283c81c79195065a5b7c32
Kevin Tun <kevin.tun@northwestern.edu>
2026-08-12 09:18:51 -0500

    add InsertImagePayload

--- src/packages/image-plugin/InsertImageCommand.ts
+++ src/packages/image-plugin/InsertImageCommand.ts
@@ -1,8 +1,20 @@
 import { createCommand } from "lexical"
 
-export type InsertImagePayload = {
+export type InsertBasicImagePayload = {
   source: string
   key?: string
 }
 
-export const INSERT_IMAGE_COMMAND = createCommand<InsertImagePayload>("INSERT_IMAGE")
+export type InsertImagePayload = {
+  source: string
+  alt?: string
+  width: number
+  height: number
+  key?: string
+  alignment: "left" | "right"
+}
+
+const INSERT_IMAGE_COMMAND = createCommand<InsertImagePayload>("INSERT_IMAGE")
+const INSERT_BASIC_IMAGE_COMMAND = createCommand<InsertBasicImagePayload>("INSERT_BASIC_IMAGE")
+
+export { INSERT_IMAGE_COMMAND, INSERT_BASIC_IMAGE_COMMAND }
